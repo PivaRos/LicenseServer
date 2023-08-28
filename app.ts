b798faf4d10cdb4ca3d30ac3558927207b300faa
PivaRos <sbhtk478@gmail.com>
2023-08-28 15:46:42 +0300

    add routers and licensing

--- app.ts
+++ app.ts
@@ -1,7 +1,18 @@
-import express from "express";
+import express, { Router } from "express";
+import cors from "cors";
+import AuthRouter from "./routers/auth";
+
+require("dotenv").config();
 
 const app = express();
 
-app.listen(9000, () => {
-  console.log("app running");
+app.use(cors());
+app.use(express.json());
+
+const APIRouter = Router();
+APIRouter.use("/auth", AuthRouter);
+app.use("/api", APIRouter);
+
+app.listen(process.env.PORT, () => {
+  console.log(`app running in port ${process.env.PORT}`);
 });
---
+++ routers/auth.ts
@@ -0,0 +1,85 @@
+import { Router } from "express";
+import {
+  param,
+  validationResult,
+  OneOfErrorType,
+  body,
+} from "express-validator";
+
+const AuthRouter = Router();
+
+var localStorage: any = null;
+if (typeof localStorage === "undefined" || localStorage === null) {
+  var LocalStorage = require("node-localstorage").LocalStorage;
+  localStorage = new LocalStorage("./scratch");
+}
+
+AuthRouter.get(
+  "/state/:SoftwareID",
+  param("SoftwareID").isMACAddress(),
+  (req, res) => {
+    try {
+      const result = validationResult(req);
+      if (!result.isEmpty()) result.throw();
+      const licenseString = localStorage.getItem(req.params?.SoftwareID);
+      if (licenseString) {
+        const License = JSON.parse(licenseString) as {
+          license: boolean;
+          name: string;
+        };
+        if (License.license === true) return res.sendStatus(200);
+        else
+          return [
+            res.status(402),
+            res.json({ message: `${License.name} is not Licensed !` }),
+          ];
+      }
+      return [
+        res.status(409),
+        res.json({ message: "no record found for license" }),
+      ];
+    } catch (e: any) {
+      if (e.errors && e.errors.length > 0)
+        switch (e.errors[0].msg) {
+          case "Invalid value":
+            return [res.status(400), res.json({ message: e.errors[0].msg })];
+        }
+      return [res.sendStatus(500)];
+    }
+  }
+);
+
+AuthRouter.post(
+  "/register",
+  body("SoftwareID").isMACAddress(),
+  body("name").isString(),
+  async (req, res) => {
+    try {
+      const result = validationResult(req);
+      if (!result.isEmpty()) result.throw();
+      const check = await localStorage.getItem(req.body.SoftwareID);
+      if (check)
+        return [
+          res.status(409),
+          res.json({ message: "this id is already registered" }),
+        ];
+      localStorage.setItem(
+        req.body.SoftwareID,
+        JSON.stringify({
+          license: false,
+          name: req.body.name,
+        })
+      );
+      return res.sendStatus(200);
+    } catch (e: any) {
+      if (e.errors && e.errors.length > 0)
+        switch (e.errors[0].msg) {
+          case "Invalid value":
+            return [res.status(400), res.json({ message: e.errors[0].msg })];
+        }
+      return [res.sendStatus(500)];
+    }
+  }
+);
+
+export default AuthRouter;
